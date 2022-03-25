d05dfd38a75a11c51349fb5ca49fe540f55ff40f
elsa manang <manangelsa07@gmail.com>
2022-03-25 09:45:38 +0100

    footer component

---
+++ src/layout/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer () {
+    return (
+        <div className="page-footer">
+			<div className="page-footer-inner"> 2022 &copy; Vinco E-Learning
+			</div>
+			<div className="scroll-to-top">
+				<i className="icon-arrow-up"></i>
+			</div>
+		</div>
+    )
+}

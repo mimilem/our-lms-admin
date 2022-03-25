869dd30f871c83a179340b89f3dcfd43ed271578
elsa manang <manangelsa07@gmail.com>
2022-03-25 09:55:09 +0100

    test d'affichage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import React,{ Component } from 'react';
 import Header from './layout/header';
+import Sidebar from './layout/sidebar';
+import Footer from './layout/footer';
 
 class App extends Component {
   render() {
     return (
       <div className="page-wrapper">
         <Header />
+        <Sidebar />
+        <Footer />
       </div>
     );
   }

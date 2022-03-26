eea8cebc2cac55a3d6df3a956e33553f9065243c
elsa manang <manangelsa07@gmail.com>
2022-03-27 00:54:59 +0100

    routage

--- src/App.js
+++ src/App.js
@@ -4,15 +4,32 @@ import Header from './layout/header';
 import { Route, Routes } from "react-router-dom"
 import Footer from './layout/footer';
 import Home from './views/home';
+import GeneralStaff from './views/generalStaff/generalStaff';
+import Campuses from './views/campuses/campuses';
+import Calender from './views/calendar/calender';
+import Fees from './views/fees/fees';
+import Library from './views/library/library';
+import Notication from './views/generalNotication/notication';
+import Events from './views/generalEvent/events';
 
 class App extends Component {
+  state = {
+    show: false
+  }
   render() {
+    const {show} = this.state
     return (
       <div className="page-wrapper">
         <Header />
-        
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/generalstaff' element={<GeneralStaff show={show} />} />
+            <Route path='/campuses' element={<Campuses show={show} />} />
+            <Route path='/calender' element={<Calender show={show} />} />
+            <Route path='/fees' element={<Fees show={show} />} />
+            <Route path='/library' element={<Library show={show} />} />
+            <Route path='/notication' element={<Notication show={show} />} />
+            <Route path='/event' element={<Events show={show} />} />
+            <Route path='/' element={<Home show={show} />} />
           </Routes>
         <Footer />
       </div>

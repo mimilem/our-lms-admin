0086b59a88e27da8bd1a400f76f815540054f897
elsa manang <manangelsa07@gmail.com>
2022-03-27 00:56:26 +0100

    page event

---
+++ src/views/generalEvent/events.js
@@ -0,0 +1,132 @@
+import React,{Component} from "react";
+import Sidebar from "../../layout/sidebar";
+import { Link } from 'react-router-dom';
+
+export default class Events extends Component {
+
+    render() {
+        const {show} = this.props;
+        return(
+            <div className="page-content-wrapper">
+            <Sidebar show={show} />
+				<div className="page-content">
+					<div className="page-bar">
+						<div className="page-title-breadcrumb">
+							<div className="pull-left">
+								<div className="page-title">General Events</div>
+							</div>
+							<ol className="breadcrumb page-breadcrumb pull-right">
+								<li><i className="fa fa-home"></i>&nbsp;<Link className="parent-item"
+										to={'/'}>Home</Link>&nbsp;<i className="fa fa-angle-right"></i>
+								</li>
+								<li><a className="parent-item" href="#">Events</a>&nbsp;<i className="fa fa-angle-right"></i>
+								</li>
+								<li className="active">General Events</li>
+							</ol>
+						</div>
+					</div>
+					<div className="row">
+						<div className="card-box">
+							<div className="card-head">
+								<header>All Events</header>
+                                <div className="pull-right">
+                                <button type="button"
+									className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect m-b-10 btn-info m-3">Add Event</button>
+                                </div>
+							</div>
+							<div className="card-body ">
+								
+								<div className="row">
+									<div className="col-lg-3 col-md-6 col-12 col-sm-6">
+										<div className="blogThumb">
+											<div className="thumb-center"><img className="img-responsive" alt="user"
+													src="../assets/img/course/course1.jpg" /></div>
+											<div className="course-box">
+												<h4>Campus 1</h4>
+												
+												<p><span><i className="ti-alarm-clock"></i> Duration: 6 Months</span></p>
+												<p><span><i className="ti-user"></i> Professor: Jane Doe</span></p>
+												<p><span><i className="fa fa-graduation-cap"></i> Students: 200+</span></p>
+												
+                                                    <button type="button"
+													    className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect m-b-10 btn-light mx-2">
+                                                        <i className="fa fa-edit"></i>
+                                                    </button>
+                                                    <button type="button"
+													    className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect m-b-10 btn-pink mx-2">
+                                                        <i className="fa fa-trash"></i>
+                                                    </button>
+                                            </div>
+										</div>
+									</div>
+									<div className="col-lg-3 col-md-6 col-12 col-sm-6 ">
+										<div className="blogThumb">
+											<div className="thumb-center"><img className="img-responsive" alt="user"
+													src="../assets/img/course/course2.jpg" /></div>
+											<div className="course-box">
+												<h4>Campus 2</h4>
+												
+												<p><span><i className="ti-alarm-clock"></i> Duration: 6 Months</span></p>
+												<p><span><i className="ti-user"></i> Professor: Jane Doe</span></p>
+												<p><span><i className="fa fa-graduation-cap"></i> Students: 200+</span></p>
+												<button type="button"
+													    className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect m-b-10 btn-light mx-2">
+                                                        <i className="fa fa-edit"></i>
+                                                    </button>
+                                                    <button type="button"
+													    className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect m-b-10 btn-pink mx-2">
+                                                        <i className="fa fa-trash"></i>
+                                                    </button>
+											</div>
+										</div>
+									</div>
+									<div className="col-lg-3 col-md-6 col-12 col-sm-6">
+										<div className="blogThumb">
+											<div className="thumb-center"><img className="img-responsive" alt="user"
+													src="../assets/img/course/course3.jpg" /></div>
+											<div className="course-box">
+												<h4>Campus 3</h4>
+												<p><span><i className="ti-alarm-clock"></i> Duration: 6 Months</span></p>
+												<p><span><i className="ti-user"></i> Professor: Jane Doe</span></p>
+												<p><span><i className="fa fa-graduation-cap"></i> Students: 200+</span></p>
+												<button type="button"
+													    className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect m-b-10 btn-light mx-2">
+                                                        <i className="fa fa-edit"></i>
+                                                    </button>
+                                                    <button type="button"
+													    className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect m-b-10 btn-pink mx-2">
+                                                        <i className="fa fa-trash"></i>
+                                                    </button>
+                                            </div>
+										</div>
+									</div>
+									<div className="col-lg-3 col-md-6 col-12 col-sm-6">
+										<div className="blogThumb">
+											<div className="thumb-center"><img className="img-responsive" alt="user"
+													src="../assets/img/course/course4.jpg" /></div>
+											<div className="course-box">
+												<h4>Campus 4</h4>
+												
+												<p><span><i className="ti-alarm-clock"></i> Duration: 6 Months</span></p>
+												<p><span><i className="ti-user"></i> Professor: Jane Doe</span></p>
+												<p><span><i className="fa fa-graduation-cap"></i> Students: 200+</span></p>
+												<button type="button"
+													    className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect m-b-10 btn-light mx-2">
+                                                        <i className="fa fa-edit"></i>
+                                                    </button>
+                                                    <button type="button"
+													    className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect m-b-10 btn-pink mx-2">
+                                                        <i className="fa fa-trash"></i>
+                                                    </button>
+                                            </div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+        )
+    }
+}
